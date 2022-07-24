4d110295655d849fc1a10a9262e8392048c18c45
Ipoglif <Ipoglif@gmail.com>
2022-07-24 13:54:28 +0300

    f

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.json())
 app.use('/api', routers)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swagger)))
 
+console.log(process.env)
+
 app.get('/', async (req,res) => {
     await db('accounts').where({username: 'admin'})
         .then((users) => {
