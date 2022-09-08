12378790ee2835681a5f6815d272a65d11812cc4
Ipoglif <Ipoglif@gmail.com>
2022-09-08 17:29:58 +0300

    token with refresh

--- app/routes/authorization.js
+++ app/routes/authorization.js
@@ -111,12 +111,7 @@ async function refresh(req, res) {
                 .then(() => console.log('Token updated'))
         }
 
-        res.cookie('RefreshToken', tokens.refreshToken, {
-            maxAge: 30000,
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true
-        })
+        res.cookie('RefreshToken', tokens.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
 
         return res.json(tokens)
     } catch (e) {
